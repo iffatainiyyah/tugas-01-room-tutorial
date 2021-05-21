b8e9979dbc9459b6bd6c48170449de13484cffed
iffatainiyyah <iffatainiyyah@gmail.com>
2021-05-21 22:36:45 +0800

    add NoteRepository

---
+++ app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/repo/NoteRepository.kt
@@ -0,0 +1,48 @@
+package com.unhas.ac.id.room.tutorial.mynotes.repo
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.unhas.ac.id.room.tutorial.mynotes.db.AppDatabase
+import com.unhas.ac.id.room.tutorial.mynotes.db.Note
+import com.unhas.ac.id.room.tutorial.mynotes.db.NoteDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class NoteRepository(application: Application) {
+
+    private val noteDao: NoteDao?
+    private var notes: LiveData<List<Note>>? = null
+
+
+    init {
+        val db = AppDatabase.getInstance(application.applicationContext)
+        noteDao = db?.noteDao()
+        notes = noteDao?.getNotes()
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun insert(note: Note) = runBlocking {
+        this.launch(Dispatchers.IO) {
+            noteDao?.insertNote(note)
+        }
+    }
+
+    fun delete(note: Note) {
+        runBlocking {
+            this.launch(Dispatchers.IO) {
+                noteDao?.deleteNote(note)
+            }
+        }
+    }
+
+    fun update(note: Note) = runBlocking {
+        this.launch(Dispatchers.IO) {
+            noteDao?.updateNote(note)
+        }
+    }
+
+}
