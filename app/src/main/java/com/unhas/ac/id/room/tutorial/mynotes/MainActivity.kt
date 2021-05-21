a87544215b15ad30cb78e2da87634e1fd7d44fc8
iffatainiyyah <iffatainiyyah@gmail.com>
2021-05-21 23:16:08 +0800

    menghubungkan ViewModel dengan Activity

--- app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/MainActivity.kt
+++ app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.unhas.ac.id.room.tutorial.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.unhas.ac.id.room.tutorial.mynotes.model.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+
+
     }
+    private lateinit var noteViewModel: NoteViewModel
+
+
 }
