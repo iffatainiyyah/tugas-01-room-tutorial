dc154d269ac4d2620b0238a629bcb1f6baaef6c6
iffatainiyyah <iffatainiyyah@gmail.com>
2021-05-21 22:46:11 +0800

    add NoteViewModel

---
+++ app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/model/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package com.unhas.ac.id.room.tutorial.mynotes.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.unhas.ac.id.room.tutorial.mynotes.db.Note
+import com.unhas.ac.id.room.tutorial.mynotes.repo.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
