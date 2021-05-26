f8ded2a9fed7f5ebe69204c4b2fd7cf8587a37d4
iffatainiyyah <iffatainiyyah@gmail.com>
2021-05-26 11:55:57 +0800

    mengubah warna tampilan aplikasi

--- app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/MainActivity.kt
+++ app/src/main/java/com/unhas/ac/id/room/tutorial/mynotes/MainActivity.kt
@@ -8,7 +8,9 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.unhas.ac.id.room.tutorial.mynotes.model.NoteViewModel
 import com.unhas.ac.id.room.tutorial.mynotes.note.Note
 import com.unhas.ac.id.room.tutorial.mynotes.note.NoteAdapter
@@ -28,11 +30,14 @@ class MainActivity : AppCompatActivity() {
         }
         noteRV.adapter = noteAdapter
 
+
         noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
 
         noteViewModel.getNotes()?.observe(this, Observer {
             noteAdapter.setNotes(it)
         })
+
+
     }
         override fun onCreateOptionsMenu(menu: Menu?): Boolean {
             val inflater = menuInflater
